b0691e953a45fff615039533ee7eca39c66defe1
robertferrei <ferreirarobert727@gmail.com>
2024-06-22 17:23:57 -0300

    criando projeto de imoveis

---
+++ projeto_imoveis/database/migrations/2024_06_22_201848_create_imoveis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('imoveis', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',55);
+            $table->string('cpf');
+            $table->integer('creci');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('imoveis');
+    }
+};
---
+++ projeto_imoveis/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('name')
+
+<form>
+    <div class="row">
+      <div class="col">
+        <input type="text" class="form-control" placeholder="First name">
+      </div>
+      <div class="col">
+        <input type="text" class="form-control" placeholder="Last name">
+      </div>
+    </div>
+  </form>
+  
+@endsection
