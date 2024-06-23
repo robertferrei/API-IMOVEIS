344e67c75c1518391e0b10741a032178c0ae7e19
robertferrei <ferreirarobert727@gmail.com>
2024-06-23 18:41:54 -0300

    criando edit e atualizando Corretores

--- projeto_imoveis/resources/views/edit.blade.php
+++ projeto_imoveis/resources/views/edit.blade.php
@@ -3,22 +3,23 @@
 
 <div class="container">
     <div class="card p-4">
-        <h2 class="text-center mb-4">Cadastro de corretor</h2>
-        <form method="POST">
+        <h2 class="text-center mb-4">Edição de corretor</h2>
+        <form action="{{route('imoveis-update',['id'=>$imoveis->id])}}" method="POST">
           @csrf
+          @method('PUT')
             <div class="form-group row">
                 <div class="col">
                     <label for="cpf">Digite seu CPF:</label>
-                    <input type="text" class="form-control" name="cpf" id="cpf" placeholder="CPF">
+                    <input type="text" class="form-control" name="cpf" id="cpf" value="{{$imoveis->cpf}}" placeholder="CPF" >
                 </div>
                 <div class="col">
                     <label for="creci">Digite seu CRECI:</label>
-                    <input type="text" class="form-control" name="creci" id="creci" placeholder="CRECI">
+                    <input type="text" class="form-control" name="creci" id="creci" value="{{$imoveis->creci}}"  placeholder="CRECI" >
                 </div>
             </div>
             <div class="form-group">
                 <label for="nome">Digite seu nome:</label>
-                <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome">
+                <input type="text" class="form-control" name="nome" id="nome" value="{{$imoveis->nome}}" placeholder="Nome" >
             </div>
             <button type="submit" class="btn btn-primary">Atualizar</button>
         </form>
